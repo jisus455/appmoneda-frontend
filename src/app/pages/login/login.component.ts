12a8a2444f3c6d3187dbfbf1669c7e529199f744
jisus455 <jesus.espi2544@gmail.com>
2024-09-01 12:42:50 -0300

    modify design register

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,16 +1,35 @@
 import { Component } from '@angular/core';
-import {FormControl, Validators, FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {MatInputModule} from '@angular/material/input';
+
 import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
+
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule, MatButtonModule],
+  imports: [MatFormFieldModule, MatInputModule, MatButtonModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-  emailFormControl = new FormControl('', [Validators.required, Validators.email]);
+
+  client:Client = {
+    email: '',
+    password: ''
+  }
+
+  constructor() {}
+
+  login() {
+    console.log(this.client.email)
+    console.log(this.client.password)
+  }
+}
+
+
+interface Client {
+  email:string,
+  password:string,
 }
--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,12 +1,41 @@
 import { Component } from '@angular/core';
 
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+
+import { FormsModule } from '@angular/forms';
+
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [MatFormFieldModule, MatInputModule, MatButtonModule, MatSelectModule, FormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
 
+  client:Client = {
+    user: '',
+    email: '',
+    password: '',
+    country: ''
+  }
+
+  constructor() {}
+
+  register() {
+    console.log(this.client.user)
+    console.log(this.client.email)
+    console.log(this.client.password)
+    console.log(this.client.country)
+  }
+}
+
+interface Client {
+  user:string,
+  email:string,
+  password:string,
+  country:string
 }
